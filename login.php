879ead62c436565bb839731f7bf8ab443f2d6bb6
ReginaBokova87 <rinka_bokova@mail.ru>
2017-05-31 09:55:32 +0700

    Update login.php

--- login.php
+++ login.php
@@ -55,9 +55,8 @@ if ($mysqli->connect_errno) {
 		              echo  '<p><a href="private_office.php">Войти в Личный кабинет</a></p>';
 			echo '<p><a href="index.php?auth=exit">Выйти</a></p>';
                         }
-                        else echo '<p><a href="index.php">Данные были введены неверно! Попробуйте еще раз!</a></p>';
                     }
-
+                        else echo '<p><a href="index.php">Данные были введены неверно! Попробуйте еще раз!</a></p>';	
                 }
         }
 
