71508c35a345de568b03da195a90eb8b097d63bb
ReginaBokova87 <rinka_bokova@mail.ru>
2017-05-31 21:47:38 +0700

    Update login.php

--- login.php
+++ login.php
@@ -53,11 +53,12 @@ if ($mysqli->connect_errno) {
 		              echo  '<p><a href="private_office.php">Войти в Личный кабинет</a></p>';
 				echo '<p><a href="index.php?auth=exit">Выйти</a></p>';
                         }
-			    echo '<p><a href="index.php">Вход не выполнен! Попробуйте снова!</a></p>';
+			    
                     }
                         	
                 }
         }
+			echo '<p><a href="index.php">Вход не выполнен! Попробуйте снова!</a></p>';
         ?>
 		
 
